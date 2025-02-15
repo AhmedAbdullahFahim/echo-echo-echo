721e0e06be5263c7b117f5d4fd3cb7f91474d910
AhmedAbdullahFahim <ahmed.abdallah5022@gmail.com>
2025-02-16 00:00:18 +0200

    feat: home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,27 @@
-import Image from 'next/image'
+import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
-export default function Home() {
-  return <div className={styles.page}></div>
+const Home = () => {
+  return (
+    <main className={styles.container}>
+      <h2>
+        Welcome to RDI AI Arabic Voice Echo, a revolutionary service designed to
+        transform the way we interact with Arabic speech using advanced AI
+        technology. Our platform allows users to upload an Arabic audio file or
+        record their voice in real-time, after which our system processes the
+        audio and plays it back with a unique electronic voice transformation.
+        To enhance the experience, the last spoken word is echoed three times,
+        creating a futuristic and immersive effect. Whether you're experimenting
+        with speech synthesis, enhancing accessibility, or exploring AI-powered
+        voice modulation, this service is built to push the boundaries of Arabic
+        audio technology.
+      </h2>
+      <Link href='/echo'>
+        🚀 Try it now and experience the future of AI-powered Arabic speech!
+      </Link>
+    </main>
+  )
 }
+
+export default Home
